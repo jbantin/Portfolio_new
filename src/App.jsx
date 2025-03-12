e217a65b4ecb1cb3b77d62c7ecb9af324d3af34d
Jörg <jbantin@gmx.de>
2025-03-12 18:25:21 +0100

    favi

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
 import { Contact } from "./components/sections/Contact";
-import Lenis from "lenis";
-import Background from "./components/Background";
 import Stars from "./components/Stars";
 
 function App() {
