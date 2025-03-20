68dc8924c17f8fe9f47e77d903fe0b3b1ce7f307
Jörg <jbantin@gmx.de>
2025-03-20 12:12:12 +0100

    linkedin added

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,3 +1,5 @@
+import linkedinImg from "../../assets/linkedin.svg";
+
 export const Home = () => {
   return (
     <section
@@ -6,7 +8,7 @@ export const Home = () => {
     >
       <div className="text-center z-10 px-4 ">
         <h1 className=" text-7xl md:text-8xl font-black tracking-[-0.08em] mb-6 bg-gradient-to-r from-orange-600 to-orange-400 bg-clip-text text-transparent leading-right">
-           jbantin.dev
+          jbantin.dev
         </h1>
         <p className="text-gray-300 text-lg mb-8 max-w-lg mx-auto">
           I am a passionate software developer with a solid foundation in the
@@ -26,6 +28,17 @@ export const Home = () => {
             className="border border-orange-500/50 text-orange-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-orange-200/10"
           >
             Contact Me
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/jbntn"
+            className=" text-orange-500 font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-orange-200/10"
+          >
+            <img
+              className="w-10 pt-1"
+              src={linkedinImg}
+              alt="link to linkedin"
+            />
           </a>
         </div>
       </div>
